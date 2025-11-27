e1e32dadec81a21a2d4440f5fb25292a5b39569a
Pogith <gusdh77@naver.com>
2025-11-27 17:58:32 +0900

    feat: tanstack query 초기 세팅
    - ReactQueryDevtools 연동
    - App 전역에 QueryClientProvider로 라우터를 감싸도록 구조 추가
    
    Signed-off-by: Pogith <gusdh77@naver.com>

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,15 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { AppRouter } from './router';
 
 function App() {
+  const queryClient = new QueryClient();
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <AppRouter />
-    </>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
 
